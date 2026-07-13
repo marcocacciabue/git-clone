c088fd6a8f05e9151b7288e332fc62ae55c2bda7
Marco Cacciabue <64919062+marcocacciabue@users.noreply.github.com>
2026-07-13 14:44:47 -0300

    Update gitclone.sh

--- gitclone.sh
+++ gitclone.sh
@@ -5,6 +5,9 @@
 # created 03-04-2014
 # revised 01-01-2022
 
+### modified 13-07-2026 by Marco Cacciabue to add secure token (to get private repos) 
+### and use ssh instead of https
+
 # prettify the console output
 echo_r() {
   echo -e "\e[31m${1}\e[0m"
@@ -24,11 +27,13 @@ GITCMD="git clone "
 echo_r 'Enter the user or org name whose repositories you want to clone: '
 read GITORG
 
+echo_r 'Enter user token: '
+read GITTOKEN
+
 REPOLIST=""
-# max repos downloaded from a given user will be 100 per page x 4 pages. should be enough but increase if necessary 
-for PAGE in {1..4}; do
-  REPOLIST+=`curl --silent "https://api.github.com/users/${GITORG}/repos?per_page=100&page=${PAGE}" -q|sort|grep "\"clone_url\""|awk -F': "' '{print $2}' | sed -e 's/",//g'`
-done
+
+REPOLIST+=`curl --silent -H "Authorization: bearer ${GITTOKEN}" "https://api.github.com/search/repositories?q=user:${GITORG}" -q|sort|grep "\"ssh_url\""|awk -F': "' '{print $2}' | sed -e 's/",//g'`
+
 
 echo_y "$REPOLIST"
 echo_r "ready to clone all the above repositories of: $GITORG"
